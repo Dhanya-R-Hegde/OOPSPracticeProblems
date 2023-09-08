ad24d4891c38be94f9e7951dc3cd00e8854e3083
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-09-09 00:43:44 +0530

    [Dhanya R Hegde] : Added Constructor

---
+++ Constructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Practice
+{
+    internal class Constructor
+    {
+
+    }
+
+    public class Person1
+    {
+        public string name;
+        public int age;
+
+        public Person1() 
+        {
+            name = "Dhanya";
+            age = 21;
+        }
+
+        //Constructor overloading
+        public Person1(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
