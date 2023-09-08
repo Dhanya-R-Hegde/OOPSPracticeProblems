f9f84627a03ba4c5ace685dd87bb0e16f2943e35
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-09-09 00:34:07 +0530

    [Dhanya R Hegde] : Added Encapsulation

---
+++ Encapsulation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Practice
+{
+    internal class Encapsulation
+    {
+
+    }
+
+    class Car
+    {
+        private int _speed;
+
+        public int speed
+        {
+            get {return _speed; }
+            set { _speed = value; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_speed);
+        }
+    }
+
+    class Person
+    {
+        private string _name;
+        private int _age;
+
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_name);
+            Console.WriteLine(_age);
+        }
+    }
+
+    public class Employee1
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public int age { get; set; }
+    }
+}
